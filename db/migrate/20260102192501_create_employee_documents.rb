3f2adf1c83ed21f8b1577630cd193741cb64fde0
Udit <83054423+udit-alcowhiz@users.noreply.github.com>
2026-01-21 23:24:16 +0530

    Fix Paperclip migration for Rails 6.1

--- db/migrate/20260102192501_create_employee_documents.rb
+++ db/migrate/20260102192501_create_employee_documents.rb
@@ -5,7 +5,12 @@ class CreateEmployeeDocuments < ActiveRecord::Migration[5.0]
       t.string :title
       t.string :document_type
       t.text :notes
-      t.attachment :file
+
+      # Paperclip-safe columns (NO t.attachment)
+      t.string   :file_file_name
+      t.string   :file_content_type
+      t.bigint   :file_file_size
+      t.datetime :file_updated_at
 
       t.timestamps
     end
